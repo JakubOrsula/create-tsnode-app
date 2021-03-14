99925aabe61afe35a2c7abe1f304e75a214a61d0
Jakub Orsula <orsula.jakub@gmail.com>
2021-03-14 15:32:06 +0100

    updated from jakub-AM4

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
 
 const fs = require('fs');
 const path = require('path');
-// const yargs = require('yargs').usage(`
-//     Usage: $0 name-of-your-app
-// `)
+const yargs = require('yargs').usage(`
+    Usage: $0 name-of-your-app
+`)
 
-// const argv = yargs.argv()
+const argv = yargs.argv()
 
-// console.log(`Hello, you argv is: ${JSON.stringify(argv)}`)
-//
-// fs.mkdirSync(argv)
+console.log(`Hello, you argv is: ${JSON.stringify(argv)}`)
 
-console.log('Hello thi is working')
+fs.mkdirSync(argv)
+
