41b3cb8a0cb2c4ee30284097cdc8bef9de9708df
Jakub Orsula <orsula.jakub@gmail.com>
2021-03-14 16:34:45 +0100

    fin

--- rsrc/index.ts
+++ rsrc/index.ts
@@ -1,5 +1,5 @@
 /**
- * If you are using webstorm go to preferences, search for eslint, and enable lint on save.
+ * TODO if you are using webstorm go to preferences, search for eslint, choose automatic configuration and enable "run eslint on save".
  * You may also have to manually configure eslint location.
  * Also change indent size to 2 if that's your thing, or edit the eslintrc.
  */
@@ -9,6 +9,7 @@ function fun() {
     return 42;
 }
 
+// remove this comment hit ctrl + s, and webstorm (eslint) will add semicolons automatically
 const obj: {name: string, age: number} = {name: "jane doe", age: 50}
 
 console.log(obj.name + fun())
