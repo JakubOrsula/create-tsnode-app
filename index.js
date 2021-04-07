1fdc7e7241cf2493ff0ca814f09fbd71ec19424e
Jakub Orsula <orsula.jakub@gmail.com>
2021-04-07 09:50:25 +0200

    templating done

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 'use strict'
 
-const path = require("path");
+const path = require('path');
 const fs = require('fs');
-const {execSync} = require("child_process");
+const {execSync} = require('child_process');
 
 if (process.argv.length < 3) {
   console.log('Specify project name please')
@@ -18,32 +18,32 @@ execSync(`npm init -y`)
 
 // this is bad, todo find a better way
 
-const contents = fs.readFileSync("package.json", {encoding: "utf-8", flag: 'r'}).replace("index.js", "index.ts");
-fs.writeFileSync("package.json", contents);
+const contents = fs.readFileSync('package.json', {encoding: 'utf-8', flag: 'r'}).replace('index.js', 'index.ts');
+fs.writeFileSync('package.json', contents);
 
 console.log('copying files');
 
 
 //todo create a build script which will inject these files
   [
-  [".eslintignore", "REPLACE.eslintignore"],
-    [".eslintrc.js", "REPLACE.eslintrc.js"],
-      ["index.ts", "REPLACEindex.ts"],
-    ["tsconfig.json", "REPLACEtsconfig.json"],
-    [".prettierrc.js", "REPLACE.prettierrc.js"]
+  ['.eslintignore', 'REPLACE.eslintignore'],
+    ['.eslintrc.js', 'REPLACE.eslintrc.js'],
+      ['index.ts', 'REPLACEindex.ts'],
+    ['tsconfig.json', 'REPLACEtsconfig.json'],
+    ['.prettierrc.js', 'REPLACE.prettierrc.js']
   ].forEach(([f, contents]) => fs.writeFileSync(f, contents)) //todo maybe parallelize
 
-console.log("installing...");
+console.log('installing...');
 
 [
-  "npm i @types/node typescript ts-node",
-  "npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/eslint-plugin-tslint @typescript-eslint/parser eslint eslint-plugin-import eslint-plugin-jsdoc eslint-plugin-prefer-arrow tslint",
+  'npm i @types/node typescript ts-node',
+  'npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/eslint-plugin-tslint @typescript-eslint/parser eslint eslint-plugin-import eslint-plugin-jsdoc eslint-plugin-prefer-arrow tslint',
 ].forEach((command) => {
   try {
     console.log(`Executing: ${command}`)
     execSync(command);
   } catch (e) {
     console.log(e)
-    console.log("Sorry something went wrong. Try to read the errors, delete created dir and try again.")
+    console.log('Sorry something went wrong. Try to read the errors, delete created dir and try again.')
     process.exit(1);
   }})
